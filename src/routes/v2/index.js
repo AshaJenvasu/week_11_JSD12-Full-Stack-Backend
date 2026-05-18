370d6206ed1823e2dbbc2b2804a38288dfc16bd5
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-05-18 11:41:12 +0700

    Update CRUD for products

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { router as usersRoutes } from "./user.route.js";
+import { router as productsRoutes } from "./product.route.js";
 // import { router as productsRoutes } from "./product.route.js";
 
 export const router = Router();
 
 router.use("/users", usersRoutes);
-// router.use("/products", productsRoutes);
+router.use("/products", productsRoutes);
 // router.use("/notes", notesRoutes);
